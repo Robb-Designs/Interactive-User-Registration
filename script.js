e91b4710a5e755c452c990cc05cc84d94d0b91ae
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-28 01:27:12 -0400

    structured the helper functions that check input validation

--- script.js
+++ script.js
@@ -13,3 +13,77 @@ const confirmErrorBox = document.getElementById("confirmPasswordError");
 
 
 // Helper functions-------------------------------------------------------
+
+// Username validation
+const validateUsername = () => {
+    let isValid = true;
+    const username = usernameInput.value.trim();
+
+    if (!username || username.length < 5) {
+        usernameErrorBox.textContent = "Your username is too short!"
+        isValid = false;
+    } else {
+        usernameErrorBox.textContent = "";
+        isValid = true;
+    }
+    return isValid;
+}
+// Email validation
+const validateEmail = () => {
+    let isValid = true;
+
+    if (emailInput.validity.valueMissing) {
+        emailErrorBox.textContent = "Email is required";
+        isValid = false;
+    } else if (emailInput.validity.typeMismatch) {
+        emailErrorBox.textContent = "Enter a valid email";
+        isValid = false;
+    } else {
+        emailErrorBox.textContent = "";
+    }
+
+    return isValid;
+}
+
+// Password validation
+const validatePassword = () => {
+    let isValid = true;
+    const password = passwordInput.value;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+
+    if (passwordInput.validity.valueMissing) {
+        passwordErrorBox.textContent = "Please enter your password";
+        isValid = false;
+    } else if (passwordInput.validity.tooShort) {
+        passwordErrorBox.textContent = "Password must be at least 8 characters";
+        isValid = false;
+    } else if (!hasUpperCase || !hasLowerCase || !hasNumber) {
+        passwordErrorBox.textContent =
+            "Must include uppercase, lowercase, and a number";
+        isValid = false;
+    } else {
+        passwordErrorBox.textContent = "";
+    }
+
+    return isValid;
+}
+
+// Confirm password validation
+const validateConfirmPassword = () =>{
+    let isValid = true;
+    const confirmPassword = confirmInput.value.trim();
+
+    if(confirmInput.validity.valueMissing){
+        confirmErrorBox.textContent = "Please confirm your password";
+        isValid = false;
+    } else if(confirmPassword !== passwordInput.value){
+        confirmErrorBox.textContent = "Passwords do not match";
+        isValid = false;
+    } else {
+        confirmErrorBox.textContent = "";
+    }
+
+    return isValid;
+}
