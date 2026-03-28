23dfc2f34ba33ebc887af563868a084bbd038589
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-28 00:08:08 -0400

    declared dom elements to variables in js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Dom variables---------------------------------------------------------
+const form = document.getElementById("registrationForm");
+const usernameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmInput = document.getElementById("confirmPassword");
+
+const usernameErrorBox = document.getElementById("usernameError");
+const emailErrorBox = document.getElementById("emailError");
+const passwordErrorBox = document.getElementById("passwordError");
+const confirmErrorBox = document.getElementById("confirmPasswordError");
+
+
+// Helper functions-------------------------------------------------------
